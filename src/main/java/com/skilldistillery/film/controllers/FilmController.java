82262fce41ef0d6720d941121dbc05f7242e08b4
Your Name <you@example.com>
2021-03-05 20:51:53 -0700

    added bean for view resolver

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -3,6 +3,8 @@ package com.skilldistillery.film.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.dao.FilmDAO;
 
@@ -15,6 +17,15 @@ public class FilmController {
 	@RequestMapping(path= {"/", "home.do"})
 	public String home() {
 		// Add list of all films to model
-		return "WEB-INF/views/index.jsp";
+		return "index";
+	}
+	
+	@RequestMapping(path= "getFilmByID.do", method = RequestMethod.GET)
+	public String getFilmByID(int filmId) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("film", filmDAO.findFilmById(filmId));
+		
+	
+		return "result";
 	}
 }
